22ffe24dc7947d4cf88d6b33b7dad38b266d50e2
howardsolutions <phungthinh0946519356@gmail.com>
2025-08-13 09:08:44 +0700

    feat: REPL

---
+++ dist/repl.js
@@ -0,0 +1,35 @@
+import { createInterface } from "readline";
+export function startREPL() {
+    // Readline interface
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: 'Pokedex > '
+    });
+    // display the prompt
+    rl.prompt();
+    rl.on("line", (line) => {
+        console.log(`Received: ${line}`);
+        const cleanedInputs = cleanInput(line);
+        if (cleanedInputs.length == 0) {
+            rl.prompt();
+            return;
+        }
+        console.log(`Your command was: ${cleanedInputs[0]}`);
+        rl.prompt();
+    });
+    // Handle process exit to close readline properly
+    rl.on("close", () => {
+        process.exit(0);
+    });
+}
+export function cleanInput(input) {
+    const words = input.split(' ');
+    // const results: string[] = []
+    // for (const word of words) {
+    //     results.push(word.trim().toLocaleLowerCase())
+    // }
+    return words
+        .map(word => word.trim().toLocaleLowerCase())
+        .filter(word => word !== '');
+}
--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,36 @@
+import { createInterface } from "readline";
+
+export function startREPL() {
+  // Readline interface
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Pokedex > '
+  })
+
+  // display the prompt
+  rl.prompt();
+
+  rl.on("line", (line) => {
+    console.log(`Received: ${line}`);
+
+    const cleanedInputs = cleanInput(line);
+
+    if (cleanedInputs.length == 0) {
+      rl.prompt();
+      return;
+    }
+    
+    console.log(`Your command was: ${cleanedInputs[0]}`);
+    rl.prompt();
+  });
+
+  // Handle process exit to close readline properly
+  rl.on("close", () => {
+    process.exit(0);
+  });
+}
+
 export function cleanInput(input: string): string[] {
   const words = input.split(' ');
   // const results: string[] = []
